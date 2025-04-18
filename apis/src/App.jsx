7030d86151f2565cb6e128ed8ec3cdf5530467a6
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-04-18 22:25:45 +0200

    Creado desde warp

--- apis/src/App.jsx
+++ apis/src/App.jsx
@@ -1,17 +1,41 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const apiKey = import.meta.env.VITE_API_KEY
+  const options = "assets/"
+  const cantidades = "&offset=20&limit=20"
+  const apiUrl = import.meta.env.VITE_API_URL + options + "?apiKey=" + apiKey + cantidades
 
+  const [criptos, setCriptos] = useState()
+  const [datos, setDatos] = useState()
+  useEffect(() => {
+    axios.get(apiUrl)
+      .then(data => {
+        //  console.log(data)
+        setDatos(data.status)
+        console.log(datos)
+        setCriptos(data.data.data)
+      })
+      .catch(error => console.log('La petición ha fallado', error))
+  }, [])
+
+  if (datos !== 200) return <p>Cargando...</p>
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1>Lista de Critpomoedas</h1>
+      <h2>Estado de la peticion: {datos}</h2>
+      <ul>
+        {criptos && criptos.map(({ name, priceUsd, id, symbol, explorer, rank }) => (
+          <li key={id} className="liCriptos">
+            <div className="rank"> <h5>Rank: {rank}</h5></div>
+            <div className="nombre"> Nombre:<h5>{name}</h5></div>
+            <div className="precio"><h5>Precio: </h5>{priceUsd}</div>
+            <div className="web"><h5>Web:</h5><a href={explorer} target="_blank"> {symbol} </a></div>
+          </li>
+        ))}
+      </ul>
 
-      </div>
     </>
   )
 }
